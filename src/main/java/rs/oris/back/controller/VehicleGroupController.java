da526be312ab97ffc703cda6d52f5916a0381713
Casper <jstrahinja@gmail.com>
2026-08-19 12:13:49 +0200

    fix(port): dodat import org.apache.tomcat.util.codec.binary.Base64 u VehicleGroupController (ispao pri spajanju konflikta)

--- src/main/java/rs/oris/back/controller/VehicleGroupController.java
+++ src/main/java/rs/oris/back/controller/VehicleGroupController.java
@@ -1,5 +1,6 @@
 package rs.oris.back.controller;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import rs.oris.back.config.security.AuthUtil;
